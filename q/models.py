4cf3b51c0ee3daf15eddd1e0a17cc41937545598
iambstha <iambstha@gmail.com>
2023-05-05 04:12:10 +0545

    Database connection

--- q/models.py
+++ q/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Quiz(models.Model):
+    topic = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Quizes'
+
+    def __str__(self):
+        return self.topic
